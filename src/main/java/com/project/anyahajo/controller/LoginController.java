2bfdbf85c8a6fd7aebccf99133f921b3938958e0
krumplix <szlatky.mark@gmail.com>
2023-03-15 17:35:55 +0100

    -Belépés oldal létrehozva -SecurityConfig updatelve, hogy ne a stock SpringSecurity Login jelenjen meg hanem a saját -Regisztrációs oldal frissítve

---
+++ src/main/java/com/project/anyahajo/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.project.anyahajo.controller;
+
+import com.project.anyahajo.model.User;
+import com.project.anyahajo.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class LoginController {
+
+
+    @Autowired
+    private AppUserRepository userRepository;
+
+    @GetMapping("/login_page")
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new User());
+        return "login_page";
+    }
+
+    @PostMapping("/login_page")
+    public String login(@ModelAttribute("user") User user) {
+        Optional<User> dbUser = userRepository.findByEmail(user.getEmail());
+        if (dbUser.isPresent() && dbUser.get().getPassword().equals(user.getPassword())) {
+            return "redirect:/home";
+        }
+        return "login_page";
+    }
+}
