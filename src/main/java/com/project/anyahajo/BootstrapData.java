9e3daf1f00d5517db5e8f0a6458394c1b87b0617
BorbelyBalazs <blaze82bass@gmail.com>
2023-04-05 14:59:57 +0200

    Targy név szín

--- src/main/java/com/project/anyahajo/BootstrapData.java
+++ src/main/java/com/project/anyahajo/BootstrapData.java
@@ -17,7 +17,7 @@ public  class BootstrapData implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
 
-        itemService.deleteAll();
+//        itemService.deleteAll();
 
         Book harryPotter = new Book();
         harryPotter.setAuthor("J.K. Rowling");
